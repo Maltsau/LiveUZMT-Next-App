8ebea0c913d1871d02c1eb09fb989df41471bb52
Maltsau <emeraldwhistler.dm@gmail.com>
2022-12-01 22:26:17 +0300

    notAllFixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import dynamic from "next/dynamic";
 import Layout from "../components/Layout";
-import React from "react";
 
-const App = ({ Component, pageProps }: AppProps) => {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
-};
-
-export default dynamic(() => Promise.resolve(App), {
-  ssr: false,
-});
+}
